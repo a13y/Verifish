ac9bf55d50d5be576f92594392e92d5e7a863035
Wyn-cmd <wwjdholiday@gmail.com>
2024-09-01 14:00:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,6 @@
 
 
 
-
 import csv
 import time
 from itertools import product
@@ -505,7 +504,5 @@ print(f'initial loading:      {var_func_time:.2f} s')
 print(f'wordlist generation:  {wordlist_time:.2f} s')
 print(f'fake url generation:  {fake_domains_time:.2f} s')
 print(f'time writing to file: {write_file_time:.2f} s')
-
-
 
 
